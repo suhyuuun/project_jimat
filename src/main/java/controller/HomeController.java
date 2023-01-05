2770cde294b2371739bf0790f3ebb8d8908a093d
suhyuuun <suhyuuuuuun@gmail.com>
2023-01-05 16:22:12 +0900

    20230105 | update

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -5,14 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import dto.DetailpageMapDTO;
 import dto.DetailpagePageDTO;
-import dto.LibmapDTO;
 import service.DetailpageMapService;
-import service.LibService;
 
 //http://localhost:8090/myapp/index.do
 //http://localhost:8090/myapp/list.do
@@ -20,21 +17,16 @@ import service.LibService;
 
 @Controller
 public class HomeController {
-	private DetailpageMapService dService;
-	private LibService lService;
+	private DetailpageMapService service;
 	private DetailpagePageDTO pdto;
 	private int currentPage;
 	
 	public HomeController() {
 
 	}
-
-	public void setService(DetailpageMapService service) {
-		this.dService = dService;
-	}
 	
-	public void setlService(LibService lService) {
-		this.lService = lService;
+	public void setService(DetailpageMapService service) {
+		this.service = service;
 	}
 
 	@RequestMapping("/index.do")
@@ -68,23 +60,6 @@ public class HomeController {
 		return "search_page_map";
 	}
 
-	//검색했을 때 페이지
-	@ResponseBody
-	@RequestMapping(value = "/libmap.do", method = RequestMethod.POST)
-	public ModelAndView libraryMap(int pageNo, int pageSize, String keyword, ModelAndView mav) {
-		int countAll = lService.f_countAllProcess(keyword);
-		int totalPage = countAll / pageSize;
-		if ((countAll % pageSize) > 0)
-			totalPage++;
-
-		List<LibmapDTO> aList = lService.f_listProcess(pageNo, pageSize, keyword);
-		mav.addObject("aList", aList);
-		mav.addObject("pageNo", pageNo);
-		mav.addObject("totalPage", totalPage);
-		mav.setViewName("jsonView");
-		return mav;
-	}
-
 	@RequestMapping("/my_page.do")
 	public String my_pageForm() {
 		return "my_page";
@@ -98,14 +73,14 @@ public class HomeController {
 	//임시 맛집리스트 페이지(추후 수정예정)
 	@RequestMapping(value = "/list.do", method = RequestMethod.GET)
 	public ModelAndView listMethod(DetailpagePageDTO pv, ModelAndView mav) {
-		int totalRecord = dService.countProcess();
+		int totalRecord = service.countProcess();
 		if (totalRecord >= 1) {
 			if (pv.getCurrentPage() == 0)
 				this.currentPage = 1;
 			else
 				this.currentPage = pv.getCurrentPage();
 			this.pdto = new DetailpagePageDTO(currentPage, totalRecord);
-			List<DetailpageMapDTO> aList = dService.listProcess(this.pdto);
+			List<DetailpageMapDTO> aList = service.listProcess(this.pdto);
 			mav.addObject("aList", aList);
 			mav.addObject("pv", this.pdto);
 		}
